03e3ed2b092611f9dcce138ffe8de2debe695547
Maiii00 <maykrop0717@gmail.com>
2026-05-25 20:51:01 +0800

    add Redis for 1m cooldown

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    options.InstanceName = "Forum_";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
